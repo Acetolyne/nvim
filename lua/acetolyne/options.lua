1821a896a07e70b99b6c4b0fed1844e960785d9b
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2024-01-28 15:05:56 -0800

    add options file and switch to lazy package manager

---
+++ lua/acetolyne/options.lua
@@ -0,0 +1,14 @@
+local options = {
+    autoindent = true,
+    smartindent = true,
+    tabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+    showtabline = 0,
+    number = true,
+    relativenumber = true,
+}
+
+for option, value in pairs(options) do
+    vim.opt[option] = value
+end
