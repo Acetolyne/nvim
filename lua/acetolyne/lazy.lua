d09385eff321fedb6e96190c441de706bf87f571
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2024-01-29 11:25:33 -0800

    testing trunk changes

--- lua/acetolyne/lazy.lua
+++ lua/acetolyne/lazy.lua
@@ -63,11 +63,11 @@ require("lazy").setup({
 		-- tag = "v0.1.1",
 		-- these are optional config arguments (defaults shown)
 		config = {
-			-- trunkPath = "trunk",
+			trunkPath = "trunk",
 			-- lspArgs = {},
 			-- formatOnSave = true,
                         -- formatOnSaveTimeout = 10, -- seconds
-			-- logLevel = "info"
+			logLevel = "debug"
 		},
 		main = "trunk",
 		dependencies = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
