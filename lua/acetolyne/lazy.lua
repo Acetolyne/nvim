4b329fc0c7bd717e819d720811b0281e7f0975d5
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2024-01-29 00:15:49 -0800

    new lazy files

--- lua/acetolyne/lazy.lua
+++ lua/acetolyne/lazy.lua
@@ -60,16 +60,17 @@ require("lazy").setup({
 		"trunk-io/neovim-trunk",
 		lazy = false,
 		-- optionally pin the version
-		-- tag = "v0.1.1",
+		tag = "v0.1.1",
 		-- these are optional config arguments (defaults shown)
 		config = {
-			-- trunkPath = "trunk",
+			trunkPath = "/usr/bin/trunk",
 			-- lspArgs = {},
-			-- formatOnSave = true,
+			formatOnSave = false,
                         -- formatOnSaveTimeout = 10, -- seconds
-			-- logLevel = "info"
+			logLevel = "debug"
 		},
 		main = "trunk",
 		dependencies = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
 	},
+    { "notjedi/nvim-rooter.lua" },
 })
