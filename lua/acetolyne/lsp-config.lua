dca8777fb481416dcc97a2d9185e1946f5c8a2e6
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2024-02-03 00:15:39 -0800

    add temporary file

---
+++ lua/acetolyne/lsp-config.lua
@@ -0,0 +1,10 @@
+-- require('mason').setup()
+-- require('mason-lspconfig').setup()
+-- require("neovim-trunk").setup()
+-- {
+
+--     formatOnSave = false,
+--     logLevel = "debug",
+--     autostart = true,
+-- })
+vim.lsp.set_log_level("debug")
