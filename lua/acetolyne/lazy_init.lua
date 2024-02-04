2fb44bbc281003faa51ea9a4f526569b30da8ef3
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2024-02-04 00:18:19 -0800

    add trunk and trouble also setup enter as select for autocomplete

---
+++ lua/acetolyne/lazy_init.lua
@@ -0,0 +1,68 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = "acetolyne.lazy",
+    change_detection = { notify = false },
+
+	{
+		"trunk-io/neovim-trunk",
+		lazy = false,
+		-- optionally pin the version
+		-- tag = "v0.1.1",
+		-- these are optional config arguments (defaults shown)
+		opts = {
+			trunkPath = "trunk",
+			-- lspArgs = {},
+			-- formatOnSave = true,
+                        -- formatOnSaveTimeout = 10, -- seconds
+			logLevel = "info"
+		},
+		main = "trunk",
+		dependencies = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
+	},
+})
+--[[
+require("lazy").setup({
+  use("~/personal/harpoon")
+
+  use("theprimeagen/vim-be-good")
+  use("theprimeagen/refactoring.nvim")
+  --use("nvim-treesitter/nvim-treesitter-context");
+
+  use {
+	  "VonHeikemen/lsp-zero.nvim",
+	  branch = "v1.x",
+	  requires = {
+		  -- LSP Support
+		  {"neovim/nvim-lspconfig"},
+		  {"williamboman/mason.nvim"},
+		  {"williamboman/mason-lspconfig.nvim"},
+
+		  -- Autocompletion
+		  {"hrsh7th/nvim-cmp"},
+		  {"hrsh7th/cmp-buffer"},
+		  {"hrsh7th/cmp-path"},
+		  {"saadparwaiz1/cmp_luasnip"},
+		  {"hrsh7th/cmp-nvim-lsp"},
+		  {"hrsh7th/cmp-nvim-lua"},
+
+		  -- Snippets
+		  {"L3MON4D3/LuaSnip"},
+		  {"rafamadriz/friendly-snippets"},
+	  }
+  }
+
+}, {})
+
+--]]
