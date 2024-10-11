0b4cf08fe6f586474f67e30c4196179caaa1f31b
Jonathan Acetolyne Langlois <actolyne@programmer.net>
2024-10-10 23:45:34 -0700

    remove some config for stability

--- lua/acetolyne/lsp-config.lua
+++ lua/acetolyne/lsp-config.lua
@@ -1,10 +1,8 @@
--- require('mason').setup()
--- require('mason-lspconfig').setup()
--- require("neovim-trunk").setup()
--- {
+require("neovim-trunk").setup()
+{
 
---     formatOnSave = false,
---     logLevel = "debug",
---     autostart = true,
--- })
+    formatOnSave = false,
+    logLevel = "debug",
+    autostart = true,
+})
 vim.lsp.set_log_level("debug")
