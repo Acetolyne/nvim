a882732b89da8e7b13a616066ecbd3a8d2bd1e92
Jonathan Langlois <jonlanglois@corporatetools.com>
2026-09-21 11:14:53 -0700

    override for yaml and yml file icons

---
+++ lua/acetolyne/plugins/devicons.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  opts = {
+    override_by_extension = {
+      yaml = { icon = "\u{e6a8}", color = "#cb171e", name = "Yaml" },
+      yml = { icon = "\u{e6a8}", color = "#cb171e", name = "Yml" },
+    },
+  },
+}
